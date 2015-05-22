0bc9cbf726bd676e31da5d467f437a7e27f2ac20
Shridhara Aithal <vshridhara@gmail.com>
2015-05-22 17:43:48 +0530

    Cosmetic Changes

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This repository contains the script for the course project of Getting and Cleaning Data course on Coursera
 
-The code to clean and prepare the data is in *run_analysis.R* file. This file contains two functions:
+The code to clean and prepare the data is in _run\_analysis.R_ file. This file contains two functions:
 
 1. runAnalysis()
 2. readAndCombine()
@@ -10,30 +10,30 @@ The code to clean and prepare the data is in *run_analysis.R* file. This file co
 ###Function: readAndCombine()
 This function takes four arguments:
 
-1. measurementPath: Path to the file that contains measurement data. Ex: test\\X_test.txt
+1. featurePath: Path to the file that contains measurement data of features. Ex: test\\X_test.txt
 2. subjectPath: Path to the file that contains subject ID. Ex: test\\subject_test.txt
 3. activityPath: Path to the file that contains activity ID. Ex: test\\y_test.txt
 4. featurecols: A dataframe that contains column index and column name of the columns that are required for the purpose of this project.
 
 The function does the following:
 
-1. Reads measurement data and retains only mean and std related columns.
+1. Reads feature-measurement data and retains only mean and std related columns.
 2. Reads subject and activity data
-3. Adds subject and activity data as columns to the measurement data
+3. Adds subject and activity data as columns to the feature-measurement data
 4. Returns the complete data
 
 ###Function: runAnalysis()
-This is the function that a user calls in order to prepare the data. This function has six steps each of which do certain activity as described below.
+This is the function that a user calls in order to prepare the data. This function has six steps each of which do certain activities as described below.
  
 Step 1: 
-* Read the "features.txt" file to get a list of measurement names
-* Prune the list to contain only the columns relating to mean and std of measurements
-* Convert the measurement names to match variable naming convention of R
+* Read the "features.txt" file to get a list of feature names
+* Prune the list to contain only the columns relating to mean and standard deviation
+* Convert the feature names to match variable naming convention of R
  
  
 Step 2:
-* Combine test data, labels, and subject files.
-* Retain only the measurement columns that contain mean and std values
+* Combine test data, activity, and subject files.
+* Retain only the feature columns that contain mean and std values
 
  
 Step 3:
@@ -47,16 +47,16 @@ Step 4:
  
 Step 5:
 * Read Activity label file
-* Merge activity label file with the merged data to assign activity label to each measurement
+* Merge activity label file with the combined data from Step 4 to assign activity label to each measurement
 * Remove ActivityID column from the data frame
-* Rearrange columns such that SubjectID and Activity are followed by measurement columns
+* Rearrange columns such that SubjectID and Activity are followed by feature columns
  
  
 Step 6:
 * Group the data by SubjectID and Activity
-* Compute mean for each measurement column
-* Convert "wide format data" to "narrow format data" by converting measurement columns
+* Compute mean for each feature column
+* Convert "wide format data" to "narrow format data" by converting feature columns
   to rows with name-value pair
-* Order the data by SubjectID and Activity
+* Order the data by SubjectID, Activity, and Feature
 * Write the data to "tidydata.txt" file
  
--- run_analysis.R
+++ run_analysis.R
@@ -18,12 +18,12 @@
 
 ##################
 # Step 1: 
-# *  Read the "features.txt" file to get a list of measurement names
-# *  Prune the list to contain only the columns relating to mean and std of measurements
-# *  Convert the measurement names to match variable naming convention of R
+# *  Read the "features.txt" file to get a list of feature names
+# *  Prune the list to contain only the columns relating to mean and standard deviation
+# *  Convert the feature names to match variable naming convention of R
 # Step 2:
-# *  Combine test data, labels, and subject files.
-# *  Retain only the measurement columns that contain mean and std values
+# *  Combine test data, activity, and subject files.
+# *  Retain only the feature columns that contain mean and std values
 # Step 3:
 # *  Repeat the previous step with training data
 # Step 4:
@@ -31,13 +31,13 @@
 # *  Change column names to meaningful names
 # Step 5:
 # *  Read Activity label file
-# *  Merge activity label file with the merged data to assign activity label to each measurement
+# *  Merge activity label file with the combined data from Step 4 to assign activity label to each measurement
 # *  Remove ActivityID column from the data frame
-# *  Rearrange columns such that SubjectID and Activity are followed by measurement columns
+# *  Rearrange columns such that SubjectID and Activity are followed by feature columns
 # Step 6:
 # *  Group the data by SubjectID and Activity
-# *  Compute mean for each measurement column
-# *  Convert "wide format data" to "narrow format data" by converting measurement columns
+# *  Compute mean for each feature column
+# *  Convert "wide format data" to "narrow format data" by converting feature columns
 #    to rows with name-value pair
 # *  Order the data by SubjectID and Activity
 # *  Write the data to "tidydata.txt" file
@@ -45,73 +45,76 @@
 
 runAnalysis <- function()
 {
-    ## Step 1##
+    ## Step 1 ##
     # Read features.txt and find out which columns contain mean and std.
-    featurecols <- read.table("features.txt", sep = " ", stringsAsFactors = F)
-    colnames(featurecols) <- c("ColIndex", "FeatureName")
-    # Retain only the rows that contain "mean" and "std" measurements
+    featurecols <- read.table("features.txt", sep = " ", stringsAsFactors = F, col.names = c("ColIndex", "FeatureName"))
+    # Retain only the rows that contain "mean" and "std" features
     featurecols <- featurecols[grep("-mean|-std", featurecols$FeatureName, ignore.case = T),]
-    # Rename measurement names to standard R variable syntax
+    # Rename feature names to standard R variable syntax
     #  Replace () with "" and replace - with _
     featurecols$FeatureName <- gsub("-", "_", gsub("\\(\\)", "", featurecols$FeatureName))
-    ## End of Step 1##
+    ## End of Step 1 ##
     
-    ## Step 2##
+    ## Step 2 ##
     # Read test data
     testx <- readAndCombine("test\\X_test.txt", "test\\subject_test.txt", "test\\y_test.txt", featurecols)
-    ## End of Step 2##
+    ## End of Step 2 ##
     
-    ## Step 3##
+    ## Step 3 ##
     # Read training data
     trainx <- readAndCombine("train\\X_train.txt", "train\\subject_train.txt", "train\\y_train.txt", featurecols)
-    ## End of Step 3##
+    ## End of Step 3 ##
     
-    ## Step 4##
+    ## Step 4 ##
     # Concatenate (merge) Training and Test datasets 
     combinedData <- rbind(testx, trainx)
+    # Remove "uncombined" data from memory.
     rm("testx", "trainx")
-    colnames(combinedData) <- c("SubjectID", "ActivityID", as.character(featurecols$FeatureName))
-    ## End of Step 4##
+    # Rename columns
+    colnames(combinedData) <- c("SubjectID", "ActivityID", featurecols$FeatureName)
+    ## End of Step 4 ##
     
-    ## Step 5##
+    ## Step 5 ##
     # Read activity labels file
     actLabel <- read.table("activity_labels.txt", col.names = c("ID", "Activity"))
     
-    # Merge Activity label data with the measurement data, remove ActivityID column
+    # Merge Activity label data with the feature-measurement data, remove ActivityID column
     combinedData <- merge(combinedData, actLabel, by.x = "ActivityID", by.y = "ID")[, -1]
     
     # Reorder columns
     combinedData <- combinedData[, c(1, 81, 2:80)]
-    ## End of Step 5##
+    ## End of Step 5 ##
     
-    ## Step 6##
+    ## Step 6 ##
     # Load dplyr and tidyr packages
     library(dplyr)
     library(tidyr)
     
     # Group data by SubjectID and Activity, 
-    #    compute the mean for each measurement, convert "wide" form to "narrow" form, and
-    #    sort by SubjectID and Activity
+    #    compute the mean for each feature, 
+    #    convert "wide" form to "narrow" form, and
+    #    sort by SubjectID, Activity, and Feature
     
     combinedData <- combinedData %>% group_by(SubjectID, Activity) %>% summarise_each("mean") %>% gather(Feature, Value, -c(SubjectID, Activity)) %>% arrange(SubjectID, Activity, Feature)
     
+    # Write the final data
     write.table(combinedData, "tidydata.txt", sep = "\t", row.names = F)
-    ## End of Step 6##
+    ## End of Step 6 ##
 }
 
 # Function readAndCombine takes 4 arguments
-# 1:3 - Paths of measurement, subject, and activity files, respectively
+# 1:3 - Paths of feature-measurement, subject, and activity files, respectively
 # 4   - Dataframe containing the columns required in the output
 
-# The function reads the files, removes unneeded columns from measurement data,
-# combines subject, activity, and measurement data and returns the combined data
-readAndCombine <- function(measurementPath, subjectPath, activityPath, featurecols)
+# The function reads the files, removes unneeded columns from feature-measurement data,
+# combines subject, activity, and feature-measurement data and returns the combined data
+readAndCombine <- function(featurePath, subjectPath, activityPath, featurecols)
 {
     # Explicitly specify column classes to make read faster.
     colCls <- rep("numeric", 561)
     
-    # Read measurement data
-    meas <- read.table(measurementPath, sep = "", colClasses = colCls)
+    # Read feature data
+    meas <- read.table(featurePath, sep = "", colClasses = colCls)
     
     # Remove all columns except mean and std columns
     meas <- meas[, featurecols[,"ColIndex"]]
@@ -123,7 +126,7 @@ readAndCombine <- function(measurementPath, subjectPath, activityPath, featureco
     activity <- read.table(activityPath)
     
     # Combine the above three datasets and assign the result back to meas
-    # First column in the resulting data frame is Subject ID, followed by activity ID, followed by measurements
+    # First column in the resulting data frame is Subject ID, followed by activity ID, followed by feature-measurements
     meas <- cbind(sub, activity, meas)
     
     meas
